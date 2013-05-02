883443320c6a4a9d6885596cdf08a77b8c6db42b
Sage Gerard <sage@sagegerard.com>
2013-05-02 03:18:57 -0400

    Isolate needless static page functionality into seperate controller.
    Some of the code in the items controller is from a CI tutorial,
    and the purpose of that code was to render static pages.
    
    This is not the item controller's job. The item controller renders ITEMS, not static pages.
    
    Static page rendering functionality has been separated into a new page controller

--- application/controllers/items.php
+++ application/controllers/items.php
@@ -1,29 +1,20 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-Class Items extends CI_Controller {
-	
-	public function __construct() {
+class Items extends CI_Controller
+{
+	public function __construct()
+    {
 		parent::__construct();
 		$this->load->model('items_model');
 		//$this->layout->placeholder("title","Lifecycle Building Center");
 	}
 	
-	public function view($page = "home") {
-		if ( ! file_exists('application/views/pages/'.$page.'.php')) {
-			// Whoops, we don't have a page for that!
-			show_404();
-		}
-		
-		$data['title'] = ucfirst($page); // Capitalize the first letter
+	public function index()
+    {
 		$data['items'] = $this->items_model->getAllItems();
 		
 		$this->load->view('templates/header', $data);
 		$this->load->view('items/index', $data);
 		$this->load->view('templates/footer', $data);		
-	}
-	
-	public function index() {
-		$this->load->view('templates/header');
-		$this->load->view('templates/footer');
 	}
 }

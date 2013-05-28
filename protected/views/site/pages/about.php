077352e3e9e38a949b8c98cd8ea7b336155e64e6
Sage Gerard <sage@sagegerard.com>
2013-05-28 12:36:09 -0400

    Add real about content

--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -1,5 +1,89 @@
-<h1>About LBC</h1>
+<h1>About</h1>
+<h2>Mission</h2>
+<p>The mission of the LBC is to make the lifecycle use of the built environment increasingly efficient and sustainable. This mission will be enacted in three ways.</p>
 
-<p>Create back-end vertical; strategic disintermediate aggregate ubiquitous e-services seamless compelling networks. Niches embrace plug-and-play mesh harness e-markets leverage bleeding-edge customized cross-platform best-of-breed paradigms. Architectures frictionless models, bleeding-edge aggregate embedded. Peer-to-peer, reinvent functionalities, monetize cross-media incentivize, reintermediate cross-media functionalities reinvent share, folksonomies; revolutionary scalable, widgets. Viral infomediaries syndicate, turn-key create generate integrateAJAX-enabled expedite holistic turn-key interfaces; integrate optimize feeds integrate deploy. Share action-items metrics platforms, "peer-to-peer, design maximize reinvent innovative."</p>
+<ol>
+<li>through the operation of a retail building material reuse center as a workable and self-sufficient alternative to material disposal </li>
+<li>by using proceeds from material sales to fund grassroots community outreach programs that enhance resource efficiency in the built environment</li>
+<li>by empowering the community with the educational and experiential resources needed to effectively raise market demand for green building goods and services</li>
+</ol>
 
-<p>Life-hacks synergies: cutting-edge integrated rich compelling collaborative, "extend," incentivize incentivize data-driven strategize interfaces feeds e-markets maximize communities end-to-end compelling turn-key. Implement dot-com collaborative models synergies whiteboard visionary capture intuitive systems mesh e-business solutions sexy aggregate recontextualize remix empower 24/365, viral. Embedded incubate incubate turn-key enable supply-chains methodologies infomediaries evolve podcasts iterate reinvent clicks-and-mortar, open-source dynamic bleeding-edge, synergies impactful empower harness incentivize. Scale real-time: A-list platforms scale benchmark seamless utilize B2C granular incentivize interactive transparent functionalities revolutionary syndicate? 24/7, exploit clicks-and-mortar target syndicate synergistic visionary, mindshare efficient harness enterprise vortals, rss-capable robust. Dynamic expedite, "ecologies portals design integrate synergies, transition holistic widgets; infomediaries turn-key." Holistic integrate: innovate citizen-media end-to-end integrated feeds metrics infrastructures mesh maximize optimize, "incentivize transparent: addelivery, productize transform," podcasting interfaces facilitate platforms channels integrate. Technologies ROI sexy webservices expedite, rich e-markets deliverables Cluetrain technologies schemas relationships expedite, harness. Functionalities vortals vortals, disintermediate, "social viral convergence holistic user-contributed," capture functionalities enable interactive. Solutions users webservices transparent, revolutionary niches wikis one-to-one integrate; proactive disintermediate dynamic clicks-and-mortar."</p>
+<p>More detailed information can be found within the <a href="http://www.lifecyclebuildingcenter.org/storage/2011.9.1%20LBC%20Case%20Statement.pdf">LBC Case Statement</a></p>
+
+<h2>Learn More</h2>
+<p><img src="/images/about/banner.jpg" alt=""></p>
+
+<p>Help Make a Difference Today!</p>
+
+<p>When I talk with people about the Lifecycle Building Center concept of capturing reusable building materials from the waste stream and improving resource efficiency in the built environment, they frequently tell me 'What a great idea!' or 'It just makes sense.'  And it's easy to assume that, if an idea 'makes sense,' then it will easily find the support it needs to flourish.  But it takes more than an idea to implement substantial changes.  It takes commitment, resources, and a lot of hard work.</p>
+
+<p>The LBC is facing significant challenges and needs your help.  We've accomplished a great deal in our first 18 months of operations by diverting over 125 tons of materials from area landfills, mobilizing hundreds of volunteers, completing over 40 waste audits, and developing a strong customer base.  But, moving forward, increased financial support from the Atlanta community will be essential to our success.  We are deeply grateful to everyone who has donated their time, skills, and dollars to this cause.  Our sincere gratitude also goes out to <a href="http://www.balfourbeattyus.com/" target="_blank">Balfour Beatty Construction</a> for recently donating a forklift and fuel to the LBC; this contribution has profoundly improved our ability to collect and distribute materials.</p>
+
+<p>However, there are many other critical needs yet unmet.  In order to accommodate large-scale donations, the LBC will need to add another, larger box truck to our fleet, alongside the fantastic 14' truck donated last Fall by <a href="http://southernlightingsource.com/">Southern Lighting Source</a>.  We also need to hire more warehouse staff and provide them with the resources they need to be optimally effective.  Adding these components will allow us to expand our pick-up services and increase our sales revenue.  Another important piece is the creation of an online material database, which will give our customers real-time access to our inventory, collect information about material needs in the community, and help us demonstrate our impact by tracking key metrics.  The LBC owes a HUGE thank you to <a href="http://www.atlantaga.gov/index.aspx?page=18" target="_blank">City of Atlanta Office of Sustainability</a> for funding the first phase of this project, which can be found at <a href="http://inventory.lifecyclebuildingcenter.org/">Inventory.LifecycleBuildingCenter.org</a>.</p>
+
+<p>The expansion of our warehouse facility has also greatly increased our efficiency and is helping us better serve the community.  Last Fall, we expanded from a 12,000 SF space into an adjacent 58,000SF warehouse building, for a total under-roof area of 70,000SF.  The larger building, constructed in 1914, will now need a wide range of improvements in order to be fully utilized.  While this effort will require initial investment, the beauty of the LBC model is that our operation will be fully supported by retail sales and other services within 3-5 years of implementing this scaled-up operation.</p>
+
+<p>By looking at other successful reuse programs across the country, we can project how many millions of pounds of waste will be diverted through the LBC's efforts and the hundreds of thousands of dollars the community will save each year by having access to low-cost building materials.  But perhaps the better question to ask is: how many lives will be positively affected?  How many people will be inspired to examine their choices and become better stewards of their resources?  How many people will become more self-sufficient by learning new skills and securing a better job?  How many people will be able to improve the efficiency of their home and the quality of their neighborhood?  This is not a number I can calculate, but I can promise you that it will be commensurate with the strength of our resolve.</p>
+
+<p>So far, over 1,200 of you have chosen to follow our progress and participate in this movement.  Individually, it may seem that a small contribution will not make enough of a difference but, in reality, your power lies in coming together to support what you believe in.  And your collective power becomes even stronger if you encourage others to do the same.</p>
+
+<p>It is a significant responsibility to accept your <a href="https://usgbcga.memberclicks.net/index.php?option=com_mc&amp;view=mc&amp;mcid=form_109143" target="_blank">financial contribution</a> and promise to convert it into something that will improve people's lives.  Your decision to support the LBC requires faith that we will work hard and fulfill our promises.  I ask you to take that leap of faith.  I ask you to believe in the commitment that we have made to build the capacity for a better way of life in Atlanta; one where we maximize our resources and minimize our environmental impact.</p>
+
+<p>Help us prove that the Atlanta community stands behind the Lifecycle Building Center and demonstrate your commitment today!</p>
+
+<p>Shannon Goodman<br>Executive Director</p>
+
+<p><a href="https://usgbcga.memberclicks.net/index.php?option=com_mc&amp;view=mc&amp;mcid=form_109143" target="_blank"><img src="/images/about/take-action.bmp"></a></p>
+
+<h2>Press</h2>
+
+<p><a href="http://clatl.com/atlanta/waste-becomes-wealth-in-southwest-atlanta/Content?oid=7590785" target="_blank"><img src="/images/about/cl-logo.png" alt=""></a></p>
+<p><a href="http://clatl.com/atlanta/waste-becomes-wealth-in-southwest-atlanta/Content?oid=7590785" target="_blank">Thomas Wheatley's LBC article in Creative Loafing</a></p>
+<p> <a href="http://thedecaturminute.com/2013/04/02/salvaged-materials-from-beacon-school-complex-find-new-homes-with-the-lifecycle-building-center/" target="_blank">The Decatur Minute - "</a><a href="http://thedecaturminute.com/2013/04/02/salvaged-materials-from-beacon-school-complex-find-new-homes-with-the-lifecycle-building-center/" target="_blank">Salvaged Materials from Beacon School Complex Find New Homes with the Lifecycle Building Center"</a></p>
+
+<h2>Staff &amp; Board</h2>
+
+<ul>
+<li>Shannon Goodman - Executive Director</li>
+<li>Adam Deck - Director of Operations</li>
+<li>Jimmy Mitchell (<a href="http://www.skanska.com/">Skanska</a>) - Board Chair</li>
+<li>Stanton Stafford (<a href="http://www.newcomb-boyd.com/">Newcomb &amp; Boyd</a>) - Vice Chair</li>
+<li>Josh Sprayberry (<a href="http://www.sprayberrycpa.com/">Sprayberry &amp; Company</a>) - Chief Financial Officer, Board of Directors</li>
+<li>Frank Burdette (<a href="http://www.iesve.com/">Integrated Environmental Solutions</a>) - Board of Directors</li>
+<li>Kelly Roberts (<a href="http://www.walterpmoore.com/">Walter P Moore</a>) - Board of Directors</li>
+<li>Judy Knight (<a href="http://www.seealliance.org/">Southeast Energy Efficiency Alliance</a>) - Board of Directors</li>
+<li>Jules Toraya (<a href="http://www.atlantaga.gov/index.aspx?page=153">City of Atlanta - Mayor's Office of Sustainability</a>) - Board of Directors</li>
+<li>Drew Levine (<a href="http://www.colliers.com/en-US/Atlanta">Colliers International</a>) - Board of Directors</li>
+<li>Kay Wallace (West End Merchants Coalition) - Board of Directors</li>
+<li>Amber Pelot (<a href="http://www.alston.com/">Alston &amp; Bird</a>) - Board of Directors</li>
+<li>Cynthia Poselenzny (<a href="http://www.wastegrinding.com/">Patterson Services</a>) - Advisory Board</li>
+<li>Neil Seldman (<a href="http://www.wastegrinding.com/">Institute for Local Self-Reliance</a>) - Advisory Board</li>
+<li>Todd Phillips (<a href="http://www.ncmgroup.com/">NCM Demolition</a>) - Advisory Board</li>
+<li>Ryan Gravel (<a href="http://www.perkinswill.com/">Perkins+Will</a>) - Advisory Board</li>
+<li>Alan Scoggins (<a href="http://www.leapleyconstruction.com/">Leapley Construction</a>) - Advisory Board</li>
+<li>Jarrett Keim (<a href="http://www.jedunn.com/">JE Dunn Construction</a>) - Advisory Board</li>
+<li>Tom Troutman (<a href="http://www.newsouthconstruction.com/">New South Construction</a>) - Advisory Board</li>
+<li>Peter Michelson (<a href="http://renewaldesignbuild.com/">Renewal Design Build</a>) - Advisory Board</li>
+</ul>
+
+<h2>Letters of Support</h2>
+
+<ul>
+<li><a href="http://www.lifecyclebuildingcenter.org/storage/letters-of-support/COA%20Letter%20of%20Support_LBC%20Murphy%20Ave_07-21-11.pdf">City of Atlanta</a></li>
+<li><a href="http://www.lifecyclebuildingcenter.org/storage/letters-of-support/LBC%20ABI%20Letter%20of%20Support%20July%202011.pdf">Atlanta Beltline</a></li>
+<li><a href="http://www.lifecyclebuildingcenter.org/storage/letters-of-support/USGBC-GA%20letter%20to%20Perkins%20Will.pdf">USGBC-GA</a></li>
+</ul>
+
+<p><a href="http://lifecyclebuildingcenter.blogspot.com/">Lifecycle Building Center Blog</a></p>
+
+<h2>Additional resources</h2>
+
+<ul>
+<li><a href="/storage/LBC%20article%20for%20USGBC-GA.pdf">Converting Waste Into Wealth article</a></li>
+<li><a href="http://lifecyclebuildingcenter.squarespace.com/storage/1315-peachtree-street-material-reuse-story/1315%20Material%20Reuse%20Story_BMRA.pdf">1315 Peachtree Street Material Reuse Story</a></li>
+<li><a href="http://www.seda.uk.net/assets/files/guides/dfd.pdf">Design for Deconstruction</a></li>
+<li><a href="http://www.lifecyclebuilding.org/files/Old%20to%20New%20Design%20Guide.pdf">Old To New Design Guide: Salvaged Building Materials in New Construction</a></li>
+<li><a href="http://www.cce.ufl.edu/Design%20for%20Deconstruction%20and%20Materials%20Reuse.pdf">Design for Deconstruction and Materials Reuse</a></li>
+<li><a href="https://ilbi.org/lbc/standard">Living Building Challenge</a></li>
+<li><a href="http://lifecyclebuildingcenter.squarespace.com/storage/USGBC-GAGettingtoZeroWaste.pdf">USGBC-GA Getting to Zero Waste Presentation</a></li>
+</ul>

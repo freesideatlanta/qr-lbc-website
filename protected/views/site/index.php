205bca68565fbdc57cb9344332549bc6186685b4
Sage Gerard <sage@sagegerard.com>
2013-05-28 13:12:01 -0400

    Add lots of content/styles

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,4 +1,7 @@
-<?php
-
-// header('Content-Type: text/plain');
-
+<div class="banner">
+ <img src="/images/grand-opening.jpg" alt="LBC Banner" class="image">
+ <div class="caption-box">
+ <h1 class="caption">Shop LBC. Now Online.</h1>
+ <h2 class="visible-desktop subcaption">Turning waste into wealth + opportunity, while benefitting the community.</h2>
+ </div>
+</div>

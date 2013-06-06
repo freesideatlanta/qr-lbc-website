54a3c683c9fa82bd6b9e721f14a514de2d6571dc
Sage Gerard <sage@sagegerard.com>
2013-06-05 23:32:17 -0400

    Remove some junk to get something viewable now that Bootstrap died

--- themes/default/views/layouts/main.php
+++ themes/default/views/layouts/main.php
@@ -7,76 +7,64 @@
 
     <?php
     $yapp = Yii::app();
-
     $css = $yapp->theme->baseUrl . "/css";
-
-    $yapp->bootstrap->register();
     $yapp->clientScript->registerCssFile($css."/style.css");
     ?>
 
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
-
 </head>
 
 <body>
 <div class="l-footer-pusher">
-
-    <?php $this->widget('bootstrap.widgets.TbNavbar',array(
-        'type'=>'inverse',
-        'fluid'=>true,
-        'brandUrl'=>'#',
-        'fixed'=>null,
-        'brand'=>'', // empty to keep "LBC"
-        'collapse'=>true,
+    <?php $this->widget('zii.widgets.CMenu',
+        array(
         'htmlOptions'=>array(
-            'class'=>'navbar-static-top lbc-nav'
+            'class'=>'navbar'
         ),
         'items'=>array(
             array(
-                'class'=>'bootstrap.widgets.TbMenu',
-                'htmlOptions'=>array(
-                    'class'=>'pull-right',
-                ),
+                'class'=>'zii.widgets.CMenu',
                 'items'=>array(
                     array('label'=>'Home', 'url'=>array('/site/index')),
                     array(
                         'label'=>'Shop',
                         'url'=>array('/shop/index'),
                         'itemOptions'=>array(
-                            'class'=>'hidden-desktop'
+                            'class'=>'is-hidden-desktop'
                         ),
                     ),
                     array(
                         'label'=>'Donate',
                         'url'=>array('/site/donate'),
                         'itemOptions'=>array(
-                            'class'=>'hidden-desktop'
+                            'class'=>'is-hidden-desktop'
                         ),
                     ),
                     array(
                         'label'=>'Request Materials',
                         'url'=>array('/site/request'),
                         'itemOptions'=>array(
-                            'class'=>'hidden-desktop'
+                            'class'=>'is-hidden-desktop'
                         ),
                     ),
                     array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
                     array('label'=>'Register', 'url'=>array('/site/register')),
                     array('label'=>'Contact', 'url'=>array('/site/contact')),
                     array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
-                    array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
+                    array('label'=>'Logout ('.$yapp->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!$yapp->user->isGuest)
                 ),
             ),
         ),
     )); ?>
 
-    <div class="container-fluid logo-bar visible-desktop">
+    <!--
+    <div class="logo-bar is-visible-desktop">
         <div class="row-fluid">
-            <div class="span4 visible-desktop">
+            <div class="span4 is-visible-desktop">
                 <img class="logo" src="/images/logo.jpg" />
             </div>
             <div class="span8">
-                <ul class="large-nav visible-desktop">
+                <ul class="large-nav is-visible-desktop">
                     <a class="large-nav-pic-link" href="/shop">
                         <li>
                             <img src="/images/shop.png" class="large-nav-icon" />
@@ -99,18 +87,9 @@
             </div>
         </div>
     </div>
+    -->
 
-    <?php // alternate centered logo for smaller displays ?>
-    <div class="container-fluid logo-bar hidden-desktop">
-        <div class="row-fluid">
-            <div class="span12">
-                <img class="logo" src="/images/logo.jpg" />
-            </div>
-        </div>
-    </div>
-
-
-    <div class="container-fluid l-content">
+    <div class="l-content">
         <div class="row-fluid">
             <?php
                 // put bootstrap breadcrumbs and spans in layout/views
@@ -122,8 +101,8 @@
 </div>
 
 <div class="l-footer">
-    <div class="container-fluid">
-    &copy; <?php echo date('Y'); ?> Lifecycle Building Center.
+    <div class="l-span-6">
+        &copy; <?php echo date('Y'); ?> Lifecycle Building Center.
     </div>
 </div>
 

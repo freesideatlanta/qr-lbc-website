03cf57e5e8f849d350957481d80d86e538222dcb
Sage Gerard <sage@sagegerard.com>
2013-06-07 09:49:23 -0400

    Add static thumbnail display, remove baseline grid, ignore thumb images in Git

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -5,3 +5,69 @@
  <h2 class="is-visible-desktop banner-subcaption">Turning waste into wealth + opportunity, while benefitting the community.</h2>
  </div>
 </div>
+
+<h2>Featured Inventory</h2>
+
+<ul class="thumbnails">        
+
+<li>
+    <img src="/images/thumbs/carpet-tile.jpg" alt="Carpet Tile" border="0">
+    <div class="thumbnail-caption">Carpet Tile</div>
+</li>
+
+<li>
+    <img src="/images/thumbs/display-cabinet.jpg" alt="Display Cabinets" border="0">
+    <div class="thumbnail-caption">Display Cabinets</div>
+</li>
+
+<li>
+    <img src="/images/thumbs/tji-joists.jpg" alt="TJI Joists" border="0">
+    <div class="thumbnail-caption">TJI Joists</div>
+</li>
+
+<li>
+    <img src="/images/thumbs/hogs-hair.jpg" alt="" border="0">
+    <div class="thumbnail-caption">Hogs Hair Filter Rolls</div>
+</li>
+
+<li>
+    <img src="/images/thumbs/ceramic-tile.jpg" alt="Ceramic Tiles" border="0">
+    <div class="thumbnail-caption">Ceramic Tiles</div>
+</li>
+
+<li>
+    <img src="/images/thumbs/french-doors.jpg" alt="Exterior French Doors" border="0">
+    <div class="thumbnail-caption">French Doors</div>
+</li>
+
+<li>
+    <img src="/images/thumbs/mahogany-doors.jpg" alt="Mahogany Doors" border="0">
+    <div class="thumbnail-caption">Mahogany Doors</div>
+</li>
+
+<li>
+    <img src="/images/thumbs/travertine-panels.jpg" alt="Travertine Panels" border="0">
+    <div class="thumbnail-caption">Travertine Panels</div>
+</li>
+
+<li>
+    <img src="/images/thumbs/plywood.jpg" alt="Plywood" border="0">
+    <div class="thumbnail-caption">Plywood</div>
+</li>
+
+<li>
+    <img src="/images/thumbs/residential-doors.jpg" alt="Residential Doors" border="0">
+    <div class="thumbnail-caption">Residential Doors </div>
+</li>
+
+<li>
+    <img src="/images/thumbs/tub-1.jpg" alt="Fiberglass Garden Tub" border="0">
+    <div class="thumbnail-caption">Garden Tub</div>
+</li>
+
+<li>
+    <img src="/images/thumbs/rollup-cabinets.jpg" alt="Rollup Cabinets" border="0">
+    <div class="thumbnail-caption">Rollup Cabinets</div>
+</li>
+
+</ul>

5d5e7b87ec31d45747faf2346e99864111613669
Sage Gerard <sage@sagegerard.com>
2013-07-14 04:22:04 -0400

    Fix syntax

--- protected/config/attrs.php
+++ protected/config/attrs.php
@@ -4,6 +4,63 @@
 // this particular app instance. Note that this file
 // will be regenerated when the form at /site/attributes
 // is submitted.
+//
+// Key - attribute
+// Value - array of attribute properties
 
 return array(
+    array(
+        'name'=>'tag',
+		'properties'=>array(
+			'type'=>'string'
+		),
+	),
+    array(
+		'name'=>'category',
+		'properties'=>array(
+			'type'=>'string'
+		),
+	),
+    array(
+		'name'=>'description',
+		'properties'=>array(
+			'type'=>'string'
+		),
+	),
+    array(
+		'name'=>'size',
+		'properties'=>array(
+			'type'=>'string'
+		),
+	),
+    array(
+		'name'=>'quantity',
+		'properties'=>array(
+			'type'=>'integer'
+		),
+	),
+    array(
+		'name'=>'condition',
+		'properties'=>array(
+			'type'=>'string'
+		),
+	),
+    array(
+		'name'=>'color',
+		'properties'=>array(
+			'type'=>'string'
+		),
+	),
+    array(
+		'name'=>'name',
+		'properties'=>array(
+			'type'=>'string'
+		),
+	),
+    array(
+		'name'=>'featured',
+        'properties'=>array(
+			'type'=>'boolean'
+		),
+    ),
 );

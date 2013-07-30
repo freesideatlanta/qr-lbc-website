6970c293dcb23aa32c8d86b75167ca5743ba4ceb
Sage Gerard <sage@sagegerard.com>
2013-07-30 11:50:31 +0000

    Enable category links from back end

--- themes/default/views/layouts/column2.php
+++ themes/default/views/layouts/column2.php
@@ -13,22 +13,20 @@
     <div class="sidebar-widget is-bordered">
         <div class="sidebar-widget-label">Categories</div>
         <ul class="inv-categories">
-            <li class="is-disabled">Appliances</li>
-            <li>Architectural Items</li>
-            <li>Cabinets</li>
-            <li class="is-disabled">Ceiling</li>
-            <li>Doors</li>
-            <li class="is-disabled">Electrical</li>
-            <li>Flooring</li>
-            <li class="is-disabled">Hardware</li>
-            <li class="is-disabled">Life Safety</li>
-            <li class="is-disabled">Lighting</li>
-            <li>Lumber / Wood</li>
-            <li>Office Furniture</li>
-            <li class="is-disabled">Paint</li>
-            <li>Plumbing</li>
-            <li>Storage</li>
-            <li class="is-disabled">Window</li>
+        <?php
+            $res = Yii::app()->get('/category');
+
+            foreach ($res['categories'] as $c)
+            {
+                $a = CHtml::link($c, array(
+                    '/shop',
+                    'category'=>$c
+                 ));
+
+                echo CHtml::tag('li',array(),$a);
+            }
+        
+        ?>
         </ul>
     </div>
 </div>

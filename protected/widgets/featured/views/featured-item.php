306dabb42754bcaaca2b393cae9d2c47053d6407
Sage Gerard <sage@sagegerard.com>
2013-08-08 10:40:25 +0000

    Remove featured widget. ProductWidget replaced it

--- protected/widgets/featured/views/featured-item.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* @var $asset array Asset JSON as PHP array */
-
-?>
-
-<div class="featured-item">
-    <img src="<?php echo $asset["photos"][0]; ?>" class="featured-item-thumbnail" />
-    <h2 class="featured-item-name"><?php echo $asset["name"]; ?></h2>
-</div>

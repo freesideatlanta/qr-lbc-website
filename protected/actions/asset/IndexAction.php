77282d9e3e13c6999b077ddef6bfb88e285eea44
Sage Gerard <sage@sagegerard.com>
2013-08-25 05:04:27 -0400

    Feed dummy data to asset index action

--- protected/actions/asset/IndexAction.php
+++ protected/actions/asset/IndexAction.php
@@ -5,6 +5,10 @@ class IndexAction extends CAction
     public function run()
     {
         // list all assets (paginated)
-        echo "index";
+        // $assets = Yii::app()->get("/asset");
+        $assets = require(dirname(__FILE__).'/../../phplib/dummy.php');
+        $this->controller->render('index', array(
+            'assets'=>$assets
+        ));
     }    
 }

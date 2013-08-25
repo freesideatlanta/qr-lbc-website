69ca7070ceb00343a804f72a95b36af5f7e63d5b
Sage Gerard <sage@sagegerard.com>
2013-08-24 20:07:25 -0400

    Add comments and getResponseInfo()

--- protected/behaviors/JsonRestClientBehavior.php
+++ protected/behaviors/JsonRestClientBehavior.php
@@ -1,9 +1,22 @@
 <?php
 
+/**
+ * Communicates with REST API exclusively with JSON.
+ * Interface with this class using PHP arrays,
+ * since JSON encoding and incoding are handled internally.
+ *
+ * Call getResponseInfo() for details on last call as it
+ * appears in a gall to curl_getinfo().
+ */
+
 class JsonRestClientBehavior extends CBehavior
 {
     private $_rest_client;
     public $api_prefix = "";
+
+    /**
+     * Initializes the class once it is attached to it's owner.
+     */
 
     public function attach($owner)
     {
@@ -11,10 +24,18 @@ class JsonRestClientBehavior extends CBehavior
         $this->_rest_client = new RestCurlClient();
     }
 
+    /**
+     * Converts PHP array to JSON
+     */
+
     private function _encode_params($arr)
     {
         return is_array($arr) ? json_encode($arr) : array();
     }
+
+    /**
+     * GET <route>. Returns JSON body as PHP array.
+     */
 
     public function get($route)
     {
@@ -23,6 +44,22 @@ class JsonRestClientBehavior extends CBehavior
             true
         );
     }
+
+    /**
+     * Returns details on the last call as it appears
+     * in curl_getinfo()
+     */
+
+    public function getResponseInfo()
+    {
+        return $_rest_client->response_info;
+    }
+    
+    /**
+     * POST to route. PHP array passed in $params
+     * will be sent as JSON. JSON response body
+     * will be returned as PHP array.
+     */
 
     public function post($route, $params = array())
     {
@@ -35,6 +72,12 @@ class JsonRestClientBehavior extends CBehavior
             );
     }
 
+    /**
+     * PUT to route. PHP array passed in $params
+     * will be sent as JSON. JSON response body
+     * will be returned as PHP array.
+     */
+
     public function put($route, $params = array())
     {
         return json_decode(
@@ -45,6 +88,12 @@ class JsonRestClientBehavior extends CBehavior
                 true
             );
     }
+
+    /**
+     * DELETE route. PHP array passed in $params
+     * will be sent as JSON. JSON response body
+     * will be returned as PHP array.
+     */
 
     public function delete($url, $params = array())
     {

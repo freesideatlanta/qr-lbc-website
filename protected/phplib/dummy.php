666dd89ad8eae43e7fef756973c50e9b4a0ea916
Sage Gerard <sage@sagegerard.com>
2013-08-24 21:25:29 -0400

    Shift id in dummy data

--- protected/phplib/dummy.php
+++ protected/phplib/dummy.php
@@ -1,61 +1,27 @@
 <?php
 
-return array(
-            0=>array(
-                'name'=>'Residential Corner Cabinets',
-                'summary'=>'Solid hardwood with glass insets',
-                'photos'=>array('http://inventory.lifecyclebuildingcenter.org/img/display-cabinet-md.jpg'),
-                'url'=>'/shop/item/0',
-                'attributes'=>array(
-                    'size'=>"3'x3'x95 3/4\"H",
-                    'Qty. Available'=>'2',
-                    'Color'=>'White/Pink',
-                    'Price'=>'$225',
-                ),
-            ),
-            1=>array(
-                'name'=>'TJI Joists',
-                'summary'=>'Only slightly used during 4 weeks of filming.',
-                'photos'=>array('http://inventory.lifecyclebuildingcenter.org/img/tji_joists_md.jpg'),
-                'url'=>'/shop/item/1',
-                'attributes'=>array(
-                    'size'=>'12" &amp; 16" tall',
-                    'Qty. Available'=>'380',
-                    'Price'=>'$1.20/foot for 12" units, $1.80/foot for 16" units',
-                ),
-            ),
-            2=>array(
-                'name'=>'Hog Hair Filter Rolls',
-                'summary'=>'Air Filter for furnace or central air',
-                'photos'=>array('http://inventory.lifecyclebuildingcenter.org/img/hogs-hair_md.jpg'),
-                'url'=>'/shop/item/2',
-                'attributes'=>array(
-                    'size'=>'72" x 48"',
-                    'Qty. Available'=>'49',
-                    'Price'=>'$150/roll',
-                ),
-            ),
-            3=>array(
-                'name'=>'Glazed Ceramic Tiles',
-                'summary'=>'Intesa brand',
-                'photos'=>array('http://inventory.lifecyclebuildingcenter.org/img/ceramic-tile_md.jpg'),
-                'url'=>'/shop/item/3',
-                'attributes'=>array(
-                    'size'=>'5" x 5"',
-                    'Qty. Available'=>'50/box',
-                    'Color'=>'Sage (22 boxes), green (15), yellow (17), beige (14)',
-                    'Price'=>'$2/sq.foot',
-                ),
-            ),
-            4=>array(
-                'name'=>'Exterior French Doors',
-                'summary'=>'Full-view with Low-E Glass',
-                'photos'=>array('http://inventory.lifecyclebuildingcenter.org/img/french-doors_md.jpg'),
-                'url'=>'/shop/item/4',
-                'attributes'=>array(
-                    'size'=>'61" x 93" (set) 30.5" x 93" (individual door) and 69" x 93" (set) 34.5" x 93" (individual door)',
-                    'Qty. Available'=>'3 sets at (61" x 93") and 2 sets at (69" x 93")',
-                    'Price'=>'$300/set',
-                ),
-            ),
-        );
+function rand_color() {
+        return str_pad(dechex(mt_rand(0, 0xFFFFFF)), 6, '0', STR_PAD_LEFT);
+}
+
+$dummy = array();
+
+for ($i = 0; $i < 20; ++$i)
+{
+    $h = $i + 1;
+
+    $dummy[] = array(
+        'id'=>$i,
+        'name'=>"Asset $i",
+        'summary'=>"Summary for asset $i, summary for asset $i, summary for asset $i",
+        'url'=>"/asset/$i",
+        'photos'=>array('http://dummyimage.com/600x400&text=%20/'.rand_color()),
+        'attributes'=>array(
+            'Size'=>'',
+            'Qty. Available'=>'43',
+            'Price'=>$h*100,
+        ),
+    );
+}
+
+return $dummy;

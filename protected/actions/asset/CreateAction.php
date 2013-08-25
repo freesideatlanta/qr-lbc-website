9f243e01175f07a1f020b46bedc2b961d725fe18
Sage Gerard <sage@sagegerard.com>
2013-08-25 05:02:36 -0400

    Back off from ActiveFormAction

--- protected/actions/asset/CreateAction.php
+++ protected/actions/asset/CreateAction.php
@@ -1,9 +1,61 @@
 <?php
 
-class CreateAction extends ActiveFormAction
+class CreateAction extends CAction
 {
-    public function afterGoodSubmission($model)
+    public function run()
     {
-        echo "create";
+        $model = new AssetFormModel();
+
+        // collect input
+        if(isset($_POST["AssetFormModel"]))
+        {
+            if (isset($_POST['attr-names']) && isset($_POST['attr-vals']))
+            {
+                $combined = array_combine(
+                    $_POST['attr-names'],
+                    $_POST['attr-vals']);
+
+                if ($combined === FALSE)
+                {
+                    $model->addError('pairs', 'All custom attributes must have values and'.
+                        'vise-versa. Do not leave any blanks in the table below.');
+                }
+
+                $model->pairs = $combined;
+                die(var_export($model->pairs));
+            }
+            
+            $model->attributes=$_POST["AssetFormModel"];
+
+            if (!$model->validate() ||
+                !$this->postAsset($model))
+            {
+                $this->controller->render('create',
+                    array('model'=>$model));
+            }
+        }
+        else
+        {
+            $this->controller->render('create',
+                array('model'=>$model));
+        }
+    }
+
+    public function postAsset($model)
+    {
+        try
+        {
+            // Yii::app()->uploadPhoto($model->photo);
+
+            Yii::app()->post("/asset", $model->toArray());
+        }
+        catch(Exception $e)
+        {
+            // silently return, since rendering the form
+            // again should show user error.
+            return false;
+        }
+
+        return true;
     }    
 }
--- protected/actions/asset/UpdateAction.php
+++ protected/actions/asset/UpdateAction.php
@@ -1,9 +1,45 @@
 <?php
 
-class UpdateAction extends ActiveFormAction
+class UpdateAction extends CAction
 {
-    public function afterGoodSubmission($model)
+    public function run()
     {
-        echo "update";
+        $model = new AssetFormModel();
+
+        // collect input
+        if(isset($_POST["AssetFormModel"]))
+        {
+            $model->attributes=$_POST["AssetFormModel"];
+            
+            if (!$model->validate() ||
+                !$this->putAsset($model))
+            {
+                $this->controller->render('create',
+                    array('model'=>$model));
+            }
+        }
+        else
+        {
+            $this->controller->render('create',
+                array('model'=>$model));
+        }
+    }
+
+    public function putAsset($model)
+    {
+        try
+        {
+            // Yii::app()->uploadPhoto($model->photo);
+
+            Yii::app()->put("/asset/".$model->id, $model->toArray());
+        }
+        catch(Exception $e)
+        {
+            // silently return, since rendering the form
+            // again should show user error.
+            return false;
+        }
+
+        return true;
     }    
 }

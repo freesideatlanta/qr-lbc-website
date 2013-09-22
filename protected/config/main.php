32b65276fd91933fcbf7b2ab675979b6240f2615
Sage Gerard <sage@sagegerard.com>
2013-09-22 12:15:38 -0400

    Update config to take word characters for IDs instead of integers

--- protected/config/main.php
+++ protected/config/main.php
@@ -78,7 +78,7 @@ return array(
 			'rules'=>array(
                 '<controller:\w+>/<id:\d+>'=>'<controller>/view',
                 'site/page/<view:\w+>'=>array('site/page'),
-				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
+				'<controller:\w+>/<action:\w+>/<id:\w+>'=>'<controller>/<action>',
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
 		),

709f083a2d25d0a3b750ed198d9a449d0df0036a
Sage Gerard <sage@sagegerard.com>
2013-09-22 13:14:51 -0400

    Refactor user identity for token use

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -5,29 +5,33 @@
  * It contains the authentication method that checks if the provided
  * data can identity the user.
  */
+
 class UserIdentity extends CUserIdentity
 {
+    private $_token;
+
 	/**
-	 * Authenticates a user.
-	 * The example implementation makes sure if the username and password
-	 * are both 'demo'.
-	 * In practical applications, this should be changed to authenticate
-	 * against some persistent user identity storage (e.g. database).
+	 * Authenticates user to back end.
 	 * @return boolean whether authentication succeeds.
-	 */
+     */
+
 	public function authenticate()
-	{
-		$users=array(
-			// username => password
-			'demo'=>'demo',
-			'admin'=>'admin',
-		);
-		if(!isset($users[$this->username]))
-			$this->errorCode=self::ERROR_USERNAME_INVALID;
-		elseif($users[$this->username]!==$this->password)
-			$this->errorCode=self::ERROR_PASSWORD_INVALID;
-		else
-			$this->errorCode=self::ERROR_NONE;
-		return !$this->errorCode;
-	}
+    {
+        $this->_token = QratitudeHelper::login(
+            $this->username,
+            $this->password
+        );
+        
+        return !is_null($this->_token);
+    }
+
+    /**
+     * Returns token associated with the user identity
+     * @return string Token
+     */
+
+    public function getToken()
+    {
+        return $this->_token;
+    }
 }

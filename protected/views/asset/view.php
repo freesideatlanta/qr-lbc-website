a9d2d9ce82bcd6b487c27898405ceccc2ca5264c
Sage Gerard <sage@sagegerard.com>
2013-09-22 12:19:41 -0400

    Render Asset object, not JSON rep

--- protected/views/asset/view.php
+++ protected/views/asset/view.php
@@ -9,19 +9,19 @@ $this->breadcrumbs=array(
 
 <div class="product">
     <div class="product-display">
-    <img src="<?php echo $asset['photos'][0]; ?>" class="product-image" /> 
+        <img src="<?php echo $asset->imageUrls[0]; ?>"
+             class="product-image" />
     </div>
     <div class="product-meta">
-    <h1 class="product-name"><?php echo $asset['name']; ?></h1>
-    <p class="product-summary"><?php echo $asset['summary']; ?></p>
+    <h1 class="product-name"><?php echo $asset->name; ?></h1>
+    <p class="product-summary"><?php echo $asset->summary; ?></p>
         <table class="product-attributes">
             <thead></thead>
             <tbody>
                 <?php
-                    $attrs = $asset['attributes'];
-                    foreach ($attrs as $k=>$v) {
-                        $tr  = CHtml::tag('th', array(), $k);
-                        $tr .= CHtml::tag('td', array(), $v);
+                    foreach ($asset->custom as $a) {
+                        $tr  = CHtml::tag('th', array(), $a->key);
+                        $tr .= CHtml::tag('td', array(), $a->val);
                         echo CHtml::tag('tr', array(), $tr);
                     }
 
@@ -31,10 +31,13 @@ $this->breadcrumbs=array(
     <div class="product-actions">
         <a class="button button-action"
            href="<?php echo $this->createUrl('shop/buy',
-                 array('asset'=>$asset['id'])); ?>">Buy</a>
+                 array('asset'=>$asset->id)); ?>">Buy</a>
         <a class="button button-neutral"
-           href="<?php echo $this->createUrl('site/contact'); ?>">Contact LBC</a>
+           href="<?php echo $this->createUrl('site/contact');?>">Contact</a>
 
+        <a class="button button-danger"
+           href="<?php echo $this->createUrl('asset/update',
+                 array('id'=>$asset->id)); ?>">Edit</a>
     </div>
     </div>
 </div>

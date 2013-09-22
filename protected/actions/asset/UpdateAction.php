aac807ae139f6fd59d18f86c310fad809f9f860b
Sage Gerard <sage@sagegerard.com>
2013-09-22 12:13:31 -0400

    Add code to route to update form

--- protected/actions/asset/UpdateAction.php
+++ protected/actions/asset/UpdateAction.php
@@ -1,45 +1,57 @@
 <?php
 
+/**
+ * Edits existing asset
+ */
+
 class UpdateAction extends CAction
 {
-    public function run()
+    public function run($id)
     {
-        $model = new AssetFormModel();
+        $asset = null;
+        $yii   = Yii::app();
+
+        $yii->clientScript->registerScriptFile(
+            $this->controller->createUrl('/js/asset-form.js'),
+            CClientScript::POS_END
+        );
+
+        $images = CUploadedFile::getInstancesByName('images');
 
         // collect input
-        if(isset($_POST["AssetFormModel"]))
+        if(isset(
+            $_POST["Asset"],
+            $_POST['AssetCustomAttribute']
+        ))
         {
-            $model->attributes=$_POST["AssetFormModel"];
+            // Construct replacement
+            $asset = new Asset();
             
-            if (!$model->validate() ||
-                !$this->putAsset($model))
+            $asset->id = $id;
+            $asset->populate(
+                 $_POST["Asset"],
+                 $_POST['AssetCustomAttribute'],
+                 $images
+            );
+
+            if ($asset->validate())
             {
-                $this->controller->render('create',
-                    array('model'=>$model));
+                if (!$asset->save())
+                {
+                    throw new Exception('507',
+                            "Sorry! Something broke. Please try again.");
+                }
+                else
+                {
+                    $yii->user->setFlash('success', "Asset saved!");
+                }
             }
         }
         else
         {
-            $this->controller->render('create',
-                array('model'=>$model));
+            $asset = QratitudeHelper::getAsset($id);
         }
+
+        $this->controller->render('update', array('asset'=>$asset));
     }
-
-    public function putAsset($model)
-    {
-        try
-        {
-            // Yii::app()->uploadPhoto($model->photo);
-
-            Yii::app()->put("/asset/".$model->id, $model->toArray());
-        }
-        catch(Exception $e)
-        {
-            // silently return, since rendering the form
-            // again should show user error.
-            return false;
-        }
-
-        return true;
-    }    
 }

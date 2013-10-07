9b176b6c064dfc393bf4403ec3aee6ae51c76b59
Sage Gerard <sage@sagegerard.com>
2013-10-06 20:06:43 -0400

    Dupe index test code for now

--- index-test.php
+++ index-test.php
@@ -10,6 +10,14 @@ $config=dirname(__FILE__).'/protected/config/test.php';
 
 // remove the following line when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
+// specify how many levels of call stack should be shown in each log message
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+
+// Grab extra hacky code that sucks
+foreach (glob(dirname(__FILE__).'/protected/phplib/*.php') as $f)
+{
+    require_once($f);
+}
 
 require_once($yii);
 Yii::createWebApplication($config)->run();
